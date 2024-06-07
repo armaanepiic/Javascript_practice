7f33c01fa8dfed71052e10c8a42bf0b1262e851b
Arman Hossain <armaanepiicfb17@gmail.com>
2024-06-08 00:56:26 +0600

    variables declaration

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
-var x;
-x = prompt("Enter Your Name");
-console.log(x);
-document.write(x);
+/*
+var a;
+let b;
+a = 45;
+b = "Arman";
+console.log(a);
+console.log(b);
+*/
+/*
+var a;
+let b;
+a = 45;
+console.log(a)
+a = 35.34;
+console.log(a);
+a = "Arman";
+console.log(a);
+*/
+/*
+var a = 20;
+var b = a;
+console.log(a);
+console.log(b);
+a = 30;
+console.log(a);
+console.log(b);
+*/
+/*
+var a = 20;
+var b = 10;
+var c = a + b;
+console.log(c);
+*/
+/*
+// variables are case sensitive
+var $a;
+$a = 30;
+console.log($a);
+*/
