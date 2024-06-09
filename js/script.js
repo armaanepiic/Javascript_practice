56067aafa0da443eb6abf3a67c61b439d5853dad
Arman Hossain <armaanepiicfb17@gmail.com>
2024-06-09 10:01:47 +0600

    assignment operator

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
-// Operator
+// comparison operator
 /*
-var a = 5;
-var b = 7;
-var c, d;
-c = a++;
-d = ++b;
-console.log(c);
-console.log(a);
-console.log(d);
+console.log(3 == 3); // true
+
+console.log(2 == 4); // false
+
+console.log(4 == "4"); // true
+console.log(4 !== 4); // false
+
+var a = 4;
+var b = 5;
+console.log(a == b);
+
+console.log(4 === "4"); // false for === consider with type
+
+console.log(4 > 5);
+console.log(4 > 3);
 */
-var line1 = "Hello";
-var line2 = "world";
+// logical operator
+// console.log(true || true);
 
-console.log(line1 + " " + line2); //Hello world
-
-console.log(line1 + 10 + 30); //Hello1030
-
-console.log(30 + 10 + line1); // 40Hello
-
-console.log("76" - "3"); // 73
-console.log("76" + "3"); // 763
-console.log("76" - "arman"); //NAN
+var a = 10;
+a += 2;
+console.log(a);
+var line1 = "Arman";
+line1 += " Hossain";
+console.log(line1);
