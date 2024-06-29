bf9f5d115b7dfdf194767dacd5243c9af0cac25d
Arman Hossain <armaanepiicfb17@gmail.com>
2024-06-29 19:56:42 +0600

    redeclare using var and let

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
-let carName = "Volvo";
-document.getElementById('carName').innerHTML = carName;
+// using var
+// var x = 3;
+// console.log(x); // x = 3
+// {
+//     var x = 4;
+//     console.log(x); // x = 4
 
-let x = "Arman" + " " + "Hossain";
-document.getElementById('myName').innerHTML = x;
+// }
+// console.log(x); // x = 4
 
-let y = "6" + 1 + 3;
-document.getElementById('sum1').innerHTML = y;
+// using let
+// let x = 3;
+// console.log(x); // x = 3
+// {
+//     let x = 4;
+//     console.log(x); // x = 4
 
-let z = 10 + 20 + "77";
-document.getElementById('sum2').innerHTML = z;
+// }
+// console.log(x); // x = 3
+
+
+
+
+
+
+
+
+
+// let carName = "Volvo";
+// document.getElementById('carName').innerHTML = carName;
+
+// let x = "Arman" + " " + "Hossain";
+// document.getElementById('myName').innerHTML = x;
+
+// let y = "6" + 1 + 3;
+// document.getElementById('sum1').innerHTML = y;
+
+// let z = 10 + 20 + "77";
+// document.getElementById('sum2').innerHTML = z;
