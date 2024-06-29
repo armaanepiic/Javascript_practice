3184de1da73d887cfc5b94b2aa28cedfe5c3f56c
Arman Hossain <armaanepiicfb17@gmail.com>
2024-06-30 00:27:04 +0600

    operator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+let p = 5 + 5 + "11";
+
+console.log(x); // 10
+console.log(typeof x);
+console.log(y); // 55
+console.log(typeof y);
+console.log(z); // hello5
+console.log(typeof z);
+console.log(p); // 1011
+console.log(typeof p);
+
+
+console.log(5 & 1);
+
+
+
+
+// let x = 10;
+// let y = 3;
+// x **= y;
+// document.getElementById('demo').innerHTML = x++;
+
+
+// const pi = 3.1416;
+// pi = 3.14;
+// console.log(pi); // error
+
+
+// carName = "ferari";
+// let carName;
+// console.log(carName);
+
+
+// carName = "ferari";
+// var carName;
+// console.log(carName);
+
+
+
 // using var
 // var x = 3;
 // console.log(x); // x = 3
