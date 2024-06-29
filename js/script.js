27225d9d8a458fbf2f76439f68b5a456816c221d
Arman Hossain <armaanepiicfb17@gmail.com>
2024-06-29 17:19:13 +0600

    variable declaration

--- js/script.js
+++ js/script.js
@@ -1,27 +1,11 @@
-// comparison operator
-/*
-console.log(3 == 3); // true
+let carName = "Volvo";
+document.getElementById('carName').innerHTML = carName;
 
-console.log(2 == 4); // false
+let x = "Arman" + " " + "Hossain";
+document.getElementById('myName').innerHTML = x;
 
-console.log(4 == "4"); // true
-console.log(4 !== 4); // false
+let y = "6" + 1 + 3;
+document.getElementById('sum1').innerHTML = y;
 
-var a = 4;
-var b = 5;
-console.log(a == b);
-
-console.log(4 === "4"); // false for === consider with type
-
-console.log(4 > 5);
-console.log(4 > 3);
-*/
-// logical operator
-// console.log(true || true);
-
-var a = 10;
-a += 2;
-console.log(a);
-var line1 = "Arman";
-line1 += " Hossain";
-console.log(line1);
+let z = 10 + 20 + "77";
+document.getElementById('sum2').innerHTML = z;
