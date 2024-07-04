37634702c5419b05f11f5f94992eaf5af2aa6161
Arman Hossain <armaanepiicfb17@gmail.com>
2024-07-04 22:56:08 +0600

    Javascript Arrays

--- js/script.js
+++ js/script.js
@@ -1,114 +1,102 @@
-// number methods
-// toString()
-// The toString() method returns a number as a string.
-/*
-let x = 123;
-console.log(typeof (x.toString())); // string
-let y = 100 + 23;
-console.log(typeof (y.toString())); // string
+
+// arrays
+const cars = ["Saab", "Volvo", "BMW"];
+document.getElementById('demo').innerHTML = cars;
+
+const items = new Array("PC", "Smart watch", "TV");
+document.getElementById('demo').innerHTML = items;
 
 
-// toExponential()
-let x = 9.65659248657;
-let y = x.toExponential(4);
-console.log(y); // 9.6566e+0
-let a = 9.659;
-let z = a.toExponential();
-console.log(z); // 9.659e+0
+// array.toString() method convert an array to a string separating the array elements
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Lichi"];
+let x = fruits.toString();
+console.log(x); // Banana,Orange,Apple,Mango -> a string
+console.log(typeof fruits);
+console.log(fruits.length);
+console.log(fruits[0]); // accessing the first element
+console.log(fruits[fruits.length - 1]);
 
 
-// toFixed()
-// toFixed() returns a string, with the number written with a specified number of decimals:
-let x = 9.656;
-console.log(x.toFixed(2)); // 9.66 it is a string
-console.log(x.toFixed(6));
-
-
-// toPrecision*(length)
-// toPrecision() returns a string, with a number written with a specified length:
-let x = 9.1256789;
-console.log(x.toPrecision(5));
-
-
-
-// Number methods
-document.getElementById('demo').innerHTML =
-    Number(true) + "<br>" +
-    Number(false) + "<br>" +
-    Number("10") + "<br>" +
-    Number("10.30") + "<br>" +
-    Number("10,30") + "<br>" +
-    Number("119 45") + "<br>" +
-    Number("ABC") + "<br>";
-
-
-let x = new Date("2024-06-24");
-document.getElementById('demo').innerHTML = Number(x);
-
-// parseInt(number)
-document.getElementById('demo').innerHTML =
-    parseInt("-10") + "<br>" + // 10
-    parseInt("-10.33") + "<br>" + // 10
-    parseInt("10") + "<br>" + // 10
-    parseInt("10.33") + "<br>" + // 10
-    parseInt("10 6") + "<br>" + // 10
-    parseInt("102 years") + "<br>" + // 10
-    parseInt("years 10"); // Nan
-
-
-// parseFloat(number)
-document.getElementById('demo').innerHTML =
-    parseFloat("10.31");
-
-
-// Number.isInteger() Method
-let x = 10;
-document.getElementById('demo').innerHTML = Number.isInteger(x); // true
-
-
-let x = 10.44;
-document.getElementById('demo').innerHTML = Number.isInteger(x); // false
-
-
-// constructor
-let num = 134.5;
-let text = num.constructor;
-console.log(text);
-
-
-let x = Number.EPSILON;
-console.log(x);
-// 2.220446049250313e-16
-// The difference between 1 and the smallest floating point number greater than 1 is:
-
-// Number.isFinite(x)
-let x = 123;
-console.log(Number.isFinite(x)); // true
-let y = "124";
-console.log(Number.isFinite(y)); // false
-*/
-
-Number.prototype.myMethod = function () {
-    return this.valueOf() / 2;
+// looping the array elements
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Lichi", "Grapes"];
+let text = "<ul>";
+for (const x of fruits) {
+    text += "<li>" + x + "</li>"; 
 }
-let x = 50;
-console.log(x.myMethod());
+text += "</ul>";
+document.getElementById('demo').innerHTML = text;
+
+
+// forEach
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Lichi", "Grapes"];
+let text = "<ul>";
+fruits.forEach(myFuncion);
+text += "</ul>";
+document.getElementById('demo').innerHTML = text;
+function myFuncion(x) {
+    text += "<li>" + x + "</li>";
+}
 
 
 
+// Adding array element
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Lichi");
+fruits.push("Grapes");
+let text = "<ul>";
+for (const x of fruits) {
+    text += "<li>" + x + "</li>";
+}
+text += "</ul>";
+document.getElementById('demo').innerHTML = text;
+
+
+// New element can also be added to an array using the length property:
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[fruits.length] = "Grapes";
+fruits[10] = "Lichi";
+console.log(fruits[fruits.length-1]);
+let text = "<ul>";
+for (const x of fruits) {
+    text += "<li>" + x + "</li>";
+}
+text += "</ul>";
+document.getElementById('demo').innerHTML = text;
+
+
+// These two different statements both create a new empty array
+const points = new Array();
+const cars = [];
+console.log(points.length);
+console.log(cars.length);
+
+
+const points = new Array(40, 50, 60, 70, 80);
+const cars = ["BMW", "Ferrari", "Audi"];
+console.log(points.length);
+console.log(cars.length);
+
+
+const points = new Array(40);
+document.getElementById('demo').innerHTML = points;  // undefinded
+console.log(points);
+const cars = new Array(40);
+document.getElementById('demo').innerHTML = cars;  // undefinded
 
 
 
+// to recognize an array
+// type of
+// Array.isArray(name)
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// type of an array is object
+console.log(typeof fruits);
+console.log(Array.isArray(fruits));
 
 
-
-
-
-
-
-
-
-
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits instanceof Array);
 
 
 
