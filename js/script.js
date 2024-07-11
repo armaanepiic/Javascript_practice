a8ebe134616a48210ff8592c589495c6e7b0d109
Arman Hossain <armaanepiicfb17@gmail.com>
2024-07-11 23:14:28 +0600

    number data types

--- js/script.js
+++ js/script.js
@@ -1,14 +1,82 @@
-const body = document.getElementById("circle");
-const text = document.getElementsByTagName("h3")[0];
-function setColor(name) {
-    body.style.background = name;
-    text.style.color = name;
-}
-function randomColor() {
-    const red = Math.round(Math.random() * 255);
-    const green = Math.round(Math.random() * 255);
-    const blue = Math.round(Math.random() * 255);
-    const color = `rgb(${red}, ${green}, ${blue})`;
-    body.style.background = color;
-    text.style.color = color;
-}
+// Data Types
+/*
+let x = 20 + 10 + "Arman";
+console.log(x);
+
+let x = "20" - "5";
+console.log(x);
+console.log(x / 0);
+
+let x = 123 * 10 ** 5;
+console.log(x);
+
+let x = 123e5;
+console.log(x);
+let y = 123e-5;
+console.log(y);
+
+let x = 10 + 20 + "Arman";
+console.log(x);
+
+let y = "The result is " + 10 + 20;
+console.log(y);
+
+console.log("20" % "4");
+
+let x = 25 / 0;
+console.log(x);
+let y = Infinity;
+console.log(y);
+
+
+// toString()
+let x = 18.567;
+console.log(typeof x);
+let y = x.toString();
+console.log(y);
+console.log(typeof y);
+
+// 187 to its binary
+
+let x = 187;
+let y = x.toString(2);
+console.log(y);
+console.log(typeof y);
+
+let x = 12.2589;
+let y = x.toPrecision(4);
+console.log(y);
+console.log(typeof y); // string
+
+let x = 187.54;
+console.log(x);
+let y = x.toString();
+console.log(y);
+console.log(typeof y);
+let z = parseFloat(y);
+console.log(z);
+console.log(typeof z);
+
+let x = "Arman";
+console.log(isNaN(x));
+let y = 123;
+console.log(isNaN(y));
+let p = "123"; // it considered as number
+console.log(isNaN(p));
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
