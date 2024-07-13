7d8144164dd9af8743e31ad15bfeef96a0761742
Arman Hossain <armaanepiicfb17@gmail.com>
2024-07-13 21:30:01 +0600

    Template literals Back tick

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
+// Template literals (ES6)
+// `` Back tick
+/*
+console.log(`First line
+Second line
+Third line
+Forth line`);
 
-//primitive type
-// number, string, boolean
-// deals with value
-
-let a = 7;
-let b = a;
-a = 45;
-console.log(b);
+let namee = "Rahim";
+let age = 25;
+let dob = "30 April 1997";
+console.log(`His name is ${namee}
+His age is ${age}
+His Date of Birth is ${dob}`);
 
 
-// reference types
-// array, object
-let numbers = [1, 2, 3];
-let newNumbers = numbers;
-console.log(newNumbers);
-numbers[1] = 800;
-console.log(newNumbers);
-
-let personal = {
-    firstName: "Arman",
-    age: 25,
-    country: "Bangladesh"
-}
-let x = personal;
-console.log(personal);
-console.log(x);
-personal.age++;
-console.log(x);
+let a = 10, b = 20;
+console.log(`${a} + ${b} = ${a + b}`);
+*/
