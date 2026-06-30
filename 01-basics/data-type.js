9f73966eb2411ba162804c5f6bc207ae8a3f37e1
arman <armaanepiic@gmail.com>
2026-06-30 20:20:11 +0600

    feat: data types readme added

---
+++ 01-basics/data-type.js
@@ -0,0 +1,11 @@
+/* 
+JavaScript data types are divided into 2 categories:
+1. Primitive Data Types (immutable) -> 7 types
+    1. String
+    2. Number
+    3. Boolean
+    4. 
+
+
+2. Object (Reference) Data Types
+*/
---
+++ 01-basics/script.js
@@ -0,0 +1,6 @@
+// variables
+var name = "Arman Hossain";
+var age = 28
+
+console.log(name)
+console.log(age)
